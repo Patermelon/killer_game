52eaab8ea6fa7a82662e0c1e48e0d181fbfef468
Huanyu Wen <Huanyu@SouthWood.local>
2016-05-18 03:06:06 -0500

    last commit seems to fail, try again.

--- server.js
+++ server.js
@@ -1,36 +1,150 @@
+//routine setup
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    users = [];
-//specify the html we will use
-app.use('/', express.static(__dirname + '/www'));
-//bind the server to the 80 port
-//server.listen(3000);//for local test
-server.listen(process.env.PORT || 3000);//publish to heroku
-//server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
-//console.log('server started on port'+process.env.PORT || 3000);
-//handle the socket
+    io = require('socket.io').listen(server);
+
+app.use('/',express.static(__dirname + '/www'));
+
+server.listen(8888);
+
+
+//define variables
+var users = [];              // the list of users' id
+    roomhostExits = 0;       // By default, there is no roomhost
+    gameModeIndicator = '8'; // By default, gameMode is 8
+
+
+
+
+/*  a simple function allowing us to toggle on/off debug messages to console.   */
+var _debugOn = 1;
+
+var _debug_logtoConsole = function(debug_msg) {
+    if (_debugOn == 1) {
+        console.log('  debug:  ' + debug_msg);
+    }
+}
+
+
+
+/*==============================================================
+        The server functionality is realized in 4 modules:
+            1. LOGIN/OUT
+            2. CHAT
+            3. LOBBY
+            4. GAME_CORE
+================================================================*/
+
+
+
 io.sockets.on('connection', function(socket) {
+    
+/*  _Module: LOGIN/OUT  */
+
+
     //new user login
-    socket.on('login', function(nickname) {
-        if (users.indexOf(nickname) > -1) {
+    socket.on('userLogin', function(id) {
+        if (users.indexOf(id) > -1) {
             socket.emit('nickExisted');
         } else {
-            socket.userIndex = users.length;
-            socket.nickname = nickname;
-            users.push(nickname);
-            socket.emit('loginSuccess');
-            io.sockets.emit('system', nickname, users.length, 'login');
-        };
+            socket.userIndex = users.length;// I wonder if .userIndex will be used in anyway, seems not
+            socket.username = id;
+            users.push(id);
+            socket.emit('loginSuccess',id); 
+            _debug_logtoConsole('users = ' + users);
+            socket.broadcast.emit('system',id,'login');
+
+            if (!roomhostExits) { //if there is no roomhost, make the current user the roomhost
+                _debug_logtoConsole('there is no roomhost currently.');
+                roomhostExits = 1;
+                _debug_logtoConsole('roomhost assigned to ' + socket.username);
+            }
+        }
     });
+
     //user leaves
     socket.on('disconnect', function() {
-        users.splice(socket.userIndex, 1);
-        socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
+        _debug_logtoConsole(users);
+        
+        var leavinguserIndex = users.indexOf(socket.username);
+        if (leavinguserIndex > -1) {
+            users.splice(leavinguserIndex, 1);
+            socket.broadcast.emit('system', socket.username, 'logout');
+            _debug_logtoConsole(socket.username+ ' has left -> ' + users);
+
+            if(leavinguserIndex == 0) { // if the current roomhost left
+                _debug_logtoConsole('current roomhost left.');
+                if (users.length <= 0) { // if the last user left
+                    roomhostExits = 0;
+                } else {
+                    _debug_logtoConsole('roomhost reassigned to ' + users[0]);
+                }
+            }
+        } else {
+            _debug_logtoConsole('a no one has left: ' + users);
+        }
     });
+
+
+
+/*  _Module: CHAT   */
+
+
     //new message get
     socket.on('postMsg', function(msg, color) {
-        socket.broadcast.emit('newMsg', socket.nickname, msg, color);
+        socket.broadcast.emit('newMsg', socket.username, msg, color);
     });
+
+    //emoji or pic posting functionality here
+
+
+
+/*  _Module: LOBBY  */
+
+
+    //user's inquiry about the roomhost
+    socket.on('amIroomhost', function() {
+        _debug_logtoConsole('heard inquiry');
+        if (users.indexOf(socket.username) == 0) {
+            socket.emit('youareroomhost', 1, gameModeIndicator);
+            _debug_logtoConsole('he is the host.');
+        } else {
+            socket.emit('youareroomhost', 0, gameModeIndicator);
+            _debug_logtoConsole('he is not the host');
+        }
+    });
+
+    //user asking for playerList
+    socket.on('pullplayerList', function() {
+        socket.emit('sendplayerList', users);
+    });
+
+    //roomhost made change to gameMode
+    socket.on('gameModeChange', function(gameMode) {
+        gameModeIndicator = gameMode;
+        socket.broadcast.emit('gameModeChanged',gameModeIndicator);
+        _debug_logtoConsole('gameMode changed to ' + gameModeIndicator);
+    });
+
+
+
+/*  _Module: GAME_CORE  */    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
--- www/scripts/killer_game.js
+++ www/scripts/killer_game.js
@@ -1,16 +1,36 @@
+// a variable to toggle the _debug_feedback()
+var _debugOn = 0;
+
+// upon loading, initializing the lobby
 window.onload = function() {
-    var game = new Game();
-    game.init();
-};
-var Game = function() {
+    var lobby = new Lobby();
+    lobby.init();
+}
+
+/*========================================================
+    the Lobby class
+
+        handles user login and the lobby functionality
+
+==========================================================*/
+
+var Lobby = function() {
     this.socket = null;
-};
-Game.prototype = {
+    var flag_imroomhost = 0;
+        myusername = '';
+        gameMode = '8';
+}//Lobby constructor
+
+
+Lobby.prototype = {
     init: function() {
         var that = this;
         this.socket = io.connect();
+        
+//  login handling
+
         this.socket.on('connect', function() {
-            document.getElementById('info').textContent = 'Please pick an ID';
+            document.getElementById('info').textContent = "欢迎光临，请输入你的昵称：";
             document.getElementById('nickWrapper').style.display = 'block';
             document.getElementById('nicknameInput').focus();
         });
@@ -19,40 +39,81 @@ Game.prototype = {
 			document.getElementById('info').textContent = 'This ID already exists.';
 		});
 
-		this.socket.on('loginSuccess',function(){
+		this.socket.on('loginSuccess',function(id){
 			document.title = '游戏中| ' + document.getElementById('nicknameInput').value;
 			document.getElementById('loginWrapper').style.display = 'none';
 			document.getElementById('messageInput').focus();
+            myusername = id;
+            that.systemLog('&quot;' + id + '&quot; 加入了房间');
+            that.socket.emit('amIroomhost');
 		});
 
-		this.socket.on('system',function(nickName,userCount,type){
-			var msg = nickName + (type == 'login' ? ' joined' : ' left');
-			var p = document.createElement('p');
-			p.textContent = msg;
-			document.getElementById('historyMsg').appendChild(p);
-			document.getElementById('status').textContent = userCount + (userCount > 1 ? ' users' : ' user') + ' online';
+		this.socket.on('system',function(id, action){
+			var msg = '&quot;' + id + '&quot;' + (action == 'login' ? ' 加入了房间' : ' 退出了房间');
+
+            that.systemLog(msg);
+
+            that.socket.emit('amIroomhost');
 		});
 
-		this.socket.on('newMsg',function(user,msg,color){
-			that._displayNewMsg(user,msg,color);
-		});
-
-		document.getElementById('loginBtn').addEventListener('click', function() {
+        document.getElementById('loginBtn').addEventListener('click', function() {
             var nickName = document.getElementById('nicknameInput').value;
             if (nickName.trim().length != 0) {
-                that.socket.emit('login', nickName);
+                that.socket.emit('userLogin', nickName.trim());
             } else {
                 document.getElementById('nicknameInput').focus();
             };
         }, false);
+
         document.getElementById('nicknameInput').addEventListener('keyup', function(e) {
             if (e.keyCode == 13) {
                 var nickName = document.getElementById('nicknameInput').value;
                 if (nickName.trim().length != 0) {
-                    that.socket.emit('login', nickName);
+                    that.socket.emit('userLogin', nickName.trim());
                 };
             };
         }, false);
+
+
+
+
+
+
+//  lobby handling
+
+        this.socket.on('youareroomhost', function(roomhostIndicator,gameModeIndicator) {
+            flag_imroomhost = roomhostIndicator;
+            gameMode = gameModeIndicator;
+
+            //that._debug_feedback('After knowing the host, showgameInfo');
+            that._showgameInfo(gameMode);
+            that.socket.emit('pullplayerList');
+        });
+
+        this.socket.on('sendplayerList', function(users) {
+            that._debug_feedback('showing playerList...');
+            that._showplayerList(users);
+        });
+
+        this.socket.on('gameModeChanged', function(gameModeIndicator) {
+            gameMode = gameModeIndicator;
+            // call chat to indicate change of gameMode
+            that._debug_feedback('gameModeChanged to ' + gameModeIndicator);
+            that._showgameInfo(gameMode);
+        });
+
+        document.getElementById('gameModeOption').addEventListener('change', function() {
+            var option_name = document.getElementById('gameModeOption');
+            that.socket.emit('gameModeChange', option_name.value);
+            gameMode = option_name.value;
+            that._displayDescription(option_name.value);
+        },false);
+
+//  chat handling
+
+        this.socket.on('newMsg',function(user,msg,color){
+            that._displayNewMsg(user,msg,color);
+        });
 
         document.getElementById('sendBtn').addEventListener('click',function() {
         	var messageInput = document.getElementById('messageInput'),
@@ -77,10 +138,13 @@ Game.prototype = {
                 that._displayNewMsg('me', msg, color);
             };
         }, false);
-	}//_fun: function(){}
+	}
+
 }
 
-Game.prototype._displayNewMsg = function(user, msg, color) {
+//  functions used in Lobby.init()
+
+Lobby.prototype._displayNewMsg = function(user, msg, color) {
     var container = document.getElementById('historyMsg'),
             msgToDisplay = document.createElement('p'),
             date = new Date().toTimeString().substr(0, 8);
@@ -89,6 +153,155 @@ Game.prototype._displayNewMsg = function(user, msg, color) {
         container.appendChild(msgToDisplay);
         container.scrollTop = container.scrollHeight;
 }
+
+Lobby.prototype._displayDescription = function(gameMode) {
+    var msgToDisplay = document.getElementById('gameDescription');
+
+    switch (gameMode) {
+        case '7':
+            msgToDisplay.innerHTML = '2警2杀1医2平';
+            break;
+        case '8':
+            msgToDisplay.innerHTML = '2警2杀1医1狙2平';
+            break;
+        default :
+    }
+}
+
+Lobby.prototype._showgameInfo = function(gameMode) {
+    if (flag_imroomhost == 1) {
+        this._debug_feedback('I am host now, should display hostWrapper.');
+        document.getElementById('roomhostWrapper').style.display = "block";
+        document.getElementById('roomguestWrapper').style.display = "none";
+        this._displayDescription(gameMode);
+    } else {
+        document.getElementById('roomhostWrapper').style.display = "none";
+        document.getElementById('roomguestWrapper').style.display = "block";
+        this._debug_feedback('generate gameModeName..');
+        
+        var gameModeName = document.getElementById('roomguestWrapper').children[0];
+        /**/
+        switch (gameMode) {
+            case '7':
+                gameModeName.innerHTML = '游戏模式：7人局';
+                break;
+            case '8':
+                gameModeName.innerHTML = '游戏模式：8人局';
+                break;
+            default :
+        }
+
+        this._displayDescription(gameMode);
+    }
+}
+
+Lobby.prototype._showplayerList = function(users) {
+    var table = document.getElementById('playerList');
+    var old_tableBody = table.childNodes[3];
+    var new_tableBody = document.createElement('tbody');
+
+    if (flag_imroomhost == 1) {
+        for (var i = 0; i < users.length; i++) {
+                var row = document.createElement('tr');
+                
+                // <td width = "150px"> i+1 </td>
+                var cell_number = document.createElement('td');
+                cell_number.setAttribute("width","150px");
+                cell_number.appendChild(document.createTextNode(i+1));
+                row.appendChild(cell_number);
+                
+                // <td width = "150px"> users[i] </td>
+                var cell_id = document.createElement('td');
+                cell_id.setAttribute("width","150px");
+                cell_id.appendChild(document.createTextNode(users[i]));
+                row.appendChild(cell_id);
+
+                // <td width = "150px"> <button id = "kicki"> kick </button> </td>
+                var cell_kickBtn = document.createElement('td');
+                cell_kickBtn.setAttribute("width","150px");
+                var kickBtn = document.createElement('button');
+                kickBtn.innerHTML = 'kick';
+                kickBtn.setAttribute("id","kick" + i);
+                kickBtn.setAttribute("style", "width: 150px; height: 40px; background-color: #cccccc;");
+                cell_kickBtn.appendChild(kickBtn);
+                row.appendChild(cell_kickBtn);
+
+                new_tableBody.appendChild(row);
+        }
+        table.replaceChild(new_tableBody,old_tableBody);
+    } else {
+        for (var i = 0; i < users.length; i++) {
+                var row = document.createElement('tr');
+                
+                // <td width = "150px"> i+1 </td>
+                var cell_number = document.createElement('td');
+                cell_number.setAttribute("width","150px");
+                cell_number.appendChild(document.createTextNode(i+1));
+                row.appendChild(cell_number);
+                
+                // <td width = "150px"> users[i] </td>
+                var cell_id = document.createElement('td');
+                cell_id.setAttribute("width","150px");
+                cell_id.appendChild(document.createTextNode(users[i]));
+                row.appendChild(cell_id);
+
+                // <td width = "150px"> <button id = "kicki"> kick </button> </td>
+                var cell_kickBtn = document.createElement('td');
+                cell_kickBtn.setAttribute("width","150px");
+                cell_kickBtn.appendChild(document.createTextNode('N/A'));
+                row.appendChild(cell_kickBtn);
+
+                new_tableBody.appendChild(row);
+        }
+        table.replaceChild(new_tableBody,old_tableBody);
+        table.children[1].children[0].children[2].innerHTML = '房主';
+    }
+
+}
+
+
+Lobby.prototype._debug_feedback = function(log) {
+    if (_debugOn == 1) {
+        var logmsg = document.createElement('p');
+        logmsg.innerHTML = log;
+        document.getElementById('historyMsg').appendChild(logmsg);
+    }
+}
+
+Lobby.prototype.systemLog = function(log) {
+        var logmsg = document.createElement('p');
+        logmsg.innerHTML = 'SYSTEM: ' + log;
+        logmsg.style.color = 'red';
+        document.getElementById('historyMsg').appendChild(logmsg);
+}
+
+
+/*=======================================================================
+    the Gameplay class
+
+        it handles the gameplay functionality on the client/browser side.
+
+=========================================================================*/
+
+var Gameplay = function() {
+    this.socket = null;
+    //var ???
+}//Gameplay constructor
+
+Gameplay.prototype = {
+    init: function() {
+        var that = this;
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
 
